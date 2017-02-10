81d5eb1c6286a0c954b5bd91af4de41d53b14d46
lukespeers <lukespeers@gmail.com>
2017-02-09 21:56:35 -0500

    added button click event

--- testApp/Form1.cs
+++ testApp/Form1.cs
@@ -16,5 +16,10 @@ namespace testApp
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Welcome to source control Imran";
+        }
     }
 }
